d3fd8a94d3955bae43d944f0df1cd9355bb73ae4
d-yokoyama <d88.yokoyama@gmail.com>
2020-03-30 13:30:29 +0900

    userとmicropostにvalidatesを追加

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,2 +1,4 @@
 class Micropost < ApplicationRecord
+  #1つのmicropostは1人のユーザーにのみ属する
+  validates :content, length: { maximum: 140 }, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  #一人のユーザーに複数のmicropostsがある
+  has_many :microposts
+  validates :name, presence: true
+  validates :email, presence: true
 end
