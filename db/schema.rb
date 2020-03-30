717bc1be0bf5aa772706a59d725fe2cc55b69ea6
d-yokoyama <d88.yokoyama@gmail.com>
2020-03-30 11:45:14 +0900

    scaffold_Userの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_30_014248) do
+ActiveRecord::Schema.define(version: 2020_03_30_020300) do
 
   create_table "posts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
